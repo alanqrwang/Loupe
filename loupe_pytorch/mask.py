39c5df7ab3b7dd47beeb158f9ba08b382485ddab
Alan Wang <aw847@en-ec-sablab-gpu02.coecis.cornell.edu>
2019-11-05 13:36:58 -0500

    added epi branch

--- loupe_pytorch/mask.py
+++ loupe_pytorch/mask.py
@@ -1,58 +1,6 @@
 import torch
 import torch.nn as nn
 
-class Mask(nn.Module):
-    def __init__(self, image_dims, pmask_slope, sample_slope, sparsity, device, straight_through_mode, eps=0.01):
-        super(Mask, self).__init__()
-        
-        self.image_dims = image_dims
-        self.pmask_slope = pmask_slope
-        self.sample_slope = sample_slope
-        self.device = device
-        self.sparsity = sparsity
-        assert straight_through_mode in ['ste-identity', 'ste-sigmoid-fixed', 'ste-sigmoid-anneal', 'relax'], \
-                       'mode should be ste-identity, ste-sigmoid-fixed, ste-sigmoid-anneal, relax'
-        self.straight_through_mode = straight_through_mode
-        
-        # MaskNet
-        # Mask is same dimension as image
-        self.pmask = nn.Parameter(torch.FloatTensor(self.image_dims[0], self.image_dims[1]))         
-        self.pmask.requires_grad = True
-        self.pmask.data.uniform_(eps, 1-eps)
-        self.pmask.data = -torch.log(1. / self.pmask.data - 1.) / self.pmask_slope
-        self.pmask.data = self.pmask.data.to(self.device)
-        
-    def squash_mask(self, mask):
-        return torch.sigmoid(self.pmask_slope*mask)
-    
-    def sparsify(self, mask):
-        xbar = mask.mean()
-        r = self.sparsity / xbar
-        beta = (1-self.sparsity) / (1-xbar)
-        le = (r <= 1).float()
-        return le * mask * r + (1-le) * (1 - (1 - mask) * beta)
-
-    def threshold(self, mask):
-        random_uniform = torch.empty(*mask.shape).uniform_(0, 1).to(self.device)
-        return torch.sigmoid(self.sample_slope*(mask - random_uniform))
-    
-    def forward(self, epoch=0, tot_epochs=0):
-        print('IN MASK')
-        # Apply probabilistic mask
-        probmask = self.squash_mask(self.pmask)
-        # Sparsify
-        sparse_mask = self.sparsify(probmask)
-        # Threshold
-        if self.straight_through_mode == 'ste-identity':
-            stidentity = straight_through_sample.STIdentity.apply
-            mask = stidentity(sparse_mask)
-        elif self.straight_through_mode == 'ste-sigmoid':
-            stsigmoid = straight_through_sample.STSigmoid.apply
-            mask = stsigmoid(sparse_mask, epoch, tot_epochs)
-        else:
-            mask = self.threshold(sparse_mask)
-        return mask
-
 class LocalConnected2d(nn.Module):
     def __init__(self, in_features):
         super(LocalConnected2d, self).__init__()
@@ -77,10 +25,8 @@ class CondMask(nn.Module):
         
         # MaskNet
         self.fc1 = nn.Linear(1, self.image_dims[0]*self.image_dims[1])
-        # self.fc2 = nn.Linear(self.image_dims[0]*self.image_dims[1], self.image_dims[0]*self.image_dims[1])
         self.relu = nn.ReLU()
         self.local1 = LocalConnected2d(self.image_dims[0]*self.image_dims[1])
-        # self.local2 = LocalConnected2d(self.image_dims[0]*self.image_dims[1])
 
     def squash_mask(self, mask):
         return torch.sigmoid(self.pmask_slope*mask)
@@ -100,7 +46,6 @@ class CondMask(nn.Module):
         return torch.sigmoid(self.sample_slope*(mask - random_uniform))
     
     def forward(self, condition, epoch=0, tot_epochs=0):
-        print('IN Cond MASK')
         fc_out = self.relu(self.fc1(condition))
         probmask = self.local1(fc_out)
 
--- loupe_pytorch/model.py
+++ loupe_pytorch/model.py
@@ -10,55 +10,11 @@ def complex_abs(x):
 
 def undersample(x, mask):
     if len(mask.shape) == 2: # This will be the case for normal Loupe
-            mask = mask.expand(x.shape[0], -1, -1)
+        mask = mask.expand(x.shape[0], -1, -1)
     undersampled_x = torch.zeros_like(x)
     undersampled_x[:,:,:,0] = torch.mul(x[:,:,:,0], mask[:,:,:])
     undersampled_x[:,:,:,1] = torch.mul(x[:,:,:,1], mask[:,:,:])
     return undersampled_x
-
-class Loupe(nn.Module):
-    def __init__(self, image_dims, pmask_slope, sample_slope, sparsity, device, straight_through_mode='relax'):
-        super(Loupe, self).__init__()
-        self.image_dims = image_dims
-        self.pmask_slope = pmask_slope
-        self.sample_slope = sample_slope
-        self.sparsity = sparsity
-        self.device = device
-
-        # Mask
-        self.mask = mask.Mask(image_dims, pmask_slope, sample_slope, sparsity, device, straight_through_mode)
-
-        # Unet
-        self.unet = unet.Unet(image_dims)
-
-    def forward(self, x):
-        # if necessary, concatenate with zeros for FFT
-        print('HIII FRIEND')
-        if x.shape[-1] == 1:
-            x = torch.cat((x, torch.zeros_like(x)), dim=3)
-
-        # input -> kspace via FFT
-        x = torch.fft(x, signal_ndim=2)
-
-        # build probability mask
-        mask = self.mask()
-        
-        # Under-sample and back to image space via IFFT
-        x = undersample(x, mask)
-        x = torch.ifft(x, signal_ndim=2).to(self.device)
-
-        # Complex absolute layer
-        abs_tensor = complex_abs(x)
-
-        # hard-coded UNet
-        x = x.view(-1, 2, self.image_dims[0], self.image_dims[1]) # Reshape for convolution
-        x = self.unet(x)
-        unet_tensor = x.view(-1, self.image_dims[0], self.image_dims[1], 2) # Reshape for convolution
-
-        # final output from model
-        out = unet_tensor + abs_tensor
-
-        return out
 
 class CondLoupe(nn.Module):
     def __init__(self, image_dims, pmask_slope, sample_slope, sparsity, device, straight_through_mode='relax'):
